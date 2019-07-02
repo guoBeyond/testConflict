fe1c9d00b69d1e4ef06cc826bcef06cad5c8fe90
Administrator <Administrator@HQ-PAB38964.sdb.local>
2019-07-02 22:19:00 +0800

    add Test2

---
+++ testMave/src/main/java/testMaven/testMave/Test2.java
@@ -0,0 +1,10 @@
+package testMaven.testMave;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is add from dev1 branch");
+	}
+
+}
