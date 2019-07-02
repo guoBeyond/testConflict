192fc5ae6e9235f01d952c8d421198f2c9830a86
Administrator <Administrator@HQ-PAB38964.sdb.local>
2019-07-02 22:38:54 +0800

    Dev2Test add

---
+++ testMave/src/main/java/testMaven/testMave/Dev2Test.java
@@ -0,0 +1,10 @@
+package testMaven.testMave;
+
+public class Dev2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is from dev2");
+	}
+
+}
