cef93ddabefe581ebb6773b94cf795ecd50c1f84
Administrator <Administrator@HQ-PAB38964.sdb.local>
2019-07-02 22:26:51 +0800

    add from master

---
+++ testMave/src/main/java/testMaven/testMave/TestMaster.java
@@ -0,0 +1,10 @@
+package testMaven.testMave;
+
+public class TestMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is add from master.");
+	}
+
+}
